c32884f647244c769e0464c4767a3326b38b526d
George Graves <gtgraves@gmail.com>
2017-03-21 16:16:49 -0500

    Assignment 8 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -19,8 +19,7 @@
         */
         var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(song);
             }
             
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -31,15 +30,24 @@
             SongPlayer.currentSong = song;
         };
         
-       /**
-       * @function playSong
-       * @desc plays song loaded in currentBuzzObject and sets song.playing to true
-       * @param {Object} song
-       */
+        /**
+        * @function playSong
+        * @desc plays song loaded in currentBuzzObject and sets song.playing to true
+        * @param {Object} song
+        */
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
         };
+        /**
+        * @function stopSong
+        * @desc stops song loaded in currentBuzzObject and sets SongPlayer.currentSong.playing to null
+        * @param {Object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            SongPlayer.currentSong.playing = null;;
+        }
         /**
         * @function getSongIndex
         * @desc gets the index number of the song passed in as argument
@@ -81,8 +89,23 @@
             currentSongIndex--;
             
             if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(stop);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        /**
+        * @method SongPlayer.next
+        * @desc assigns the index of the currently playing song to a variable and increases index by one
+        */
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if (currentSongIndex >= currentAlbum.songs.length) {
+                stopSong(song);
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
